2fdd1dcf7186fdd0d180b272795b91005ff23f77
Lvl3Mage <lvlthreemage@gmail.com>
2025-11-09 18:04:47 +0100

    Add HexGrid class and update project settings
    Introduced HexGrid.cs for hex coordinate conversions and grid logic. Updated .gitignore to exclude .idea, set RootNamespace to 'Game' in csproj, and added DotSettings for namespace inspection.

---
+++ Scripts/HexGrid.cs
@@ -0,0 +1,62 @@
+﻿using Godot;
+
+namespace Game;
+
+public partial class HexGrid : Node2D
+{
+	[Export] float cellOuterRadius = 1;
+
+
+	public float CellOuterRadius => cellOuterRadius;
+
+	public Vector2I WorldToHex(Vector2 worldCoords)
+	{
+		Vector2 localCoords = worldCoords * GlobalTransform / cellOuterRadius;
+		Vector2 hexCoords = localCoords * hexTransform;
+		return CubeToAxial(CubeRound(AxialToCubeF(hexCoords)));
+	}
+
+	Transform2D hexTransform = new(new Vector2(1.5f, Mathf.Sqrt(3f) * 0.5f), new Vector2(0f, Mathf.Sqrt(3f)),
+		Vector2.Zero);
+
+	public Vector2 HexToWorld(Vector2I hexCoords)
+	{
+		Vector2 localCoords = hexTransform * hexCoords;
+		Vector2 worldCoords = GlobalTransform * localCoords * cellOuterRadius;
+		return worldCoords;
+	}
+
+	static Vector2I CubeToAxial(Vector3I cubeCoords) => new(cubeCoords.X, cubeCoords.Y);
+	static Vector2 CubeToAxialF(Vector3 cubeCoords) => new(cubeCoords.X, cubeCoords.Y);
+
+	static Vector3I AxialToCube(Vector2I axialCoords) =>
+		new(axialCoords.X, axialCoords.Y, -axialCoords.X - axialCoords.Y);
+
+	static Vector3 AxialToCubeF(Vector2 axialCoords) =>
+		new(axialCoords.X, axialCoords.Y, -axialCoords.X - axialCoords.Y);
+
+	static Vector3I CubeRound(Vector3 fracCube)
+	{
+		Vector3I rounded = new(
+			Mathf.RoundToInt(fracCube.X),
+			Mathf.RoundToInt(fracCube.Y),
+			Mathf.RoundToInt(fracCube.Z)
+		);
+		Vector3 diff = new(
+			Mathf.Abs(rounded.X - fracCube.X),
+			Mathf.Abs(rounded.Y - fracCube.Y),
+			Mathf.Abs(rounded.Z - fracCube.Z)
+		);
+		if (diff.X > diff.Y && diff.X > diff.Z){
+			rounded.X = -rounded.Y - rounded.Z;
+		}
+		else if (diff.X > diff.Z){
+			rounded.Y = -rounded.X - rounded.Z;
+		}
+		else{
+			rounded.Z = -rounded.X - rounded.Y;
+		}
+
+		return rounded;
+	}
+}
