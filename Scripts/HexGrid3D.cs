cc5274c3221057e686c7357034ce6a6a85a4bd9b
Salix <saulpachecotrilles@gmail.com>
2025-11-25 19:29:27 +0100

    Ya hay MAPA

--- Scripts/HexGrid3D.cs
+++ Scripts/HexGrid3D.cs
@@ -6,11 +6,12 @@ namespace Game;
 [GlobalClass]
 public partial class HexGrid3D : Node3D
 {
-	[Export] float cellRadius;
+	[Export] public float CellRadius { get; private set; }
+	public int DebugDrawRadius { get; set; } = 5;
 	HexGrid grid;
 	public override void _EnterTree()
 	{
-		grid = new HexGrid(cellRadius);
+		grid = new HexGrid(CellRadius);
 	}
 
 	public Vector2I WorldToHex(Vector3 worldPosition)
@@ -30,7 +31,7 @@ public partial class HexGrid3D : Node3D
 	public void DrawCell(Vector2I cell, Color color)
 	{
 		Vector3 pos = HexToWorld(cell);
-		DebugDraw3D.DrawSphere(pos,cellRadius, color);
+		DebugDraw3D.DrawSphere(pos, CellRadius, color);
 	}
 
 	public override void _Process(double delta)
@@ -42,9 +43,9 @@ public partial class HexGrid3D : Node3D
 		// DrawCell(Vector2I.Zero + HexGrid.UpRight, Colors.Green);
 		// DrawCell(Vector2I.Zero + HexGrid.Up, Colors.Red);
 		// DrawCell(Vector2I.Zero + HexGrid.DownRight, Colors.Blue);
-		foreach (Vector2I neighborSpiralCoord in HexGrid.GetNeighborSpiralCoords(Vector2I.Zero, 3)){
+		foreach (Vector2I neighborSpiralCoord in HexGrid.GetNeighborSpiralCoords(Vector2I.Zero, DebugDrawRadius)){
 			//DebugDraw2D.SetText($"Coord {neighborSpiralCoord}");
-			DrawCell(neighborSpiralCoord,Colors.Red);
+			// DrawCell(neighborSpiralCoord,Colors.Red);
 		}
 	}
 }
--- Scripts/MapGenerator.cs
+++ Scripts/MapGenerator.cs
@@ -8,6 +8,8 @@ public partial class MapGenerator : Node3D
 
 	[Export] HexGrid3D grid;
 
+	[Export] public int MapRadius { get; private set; } = 5;
+
 	private PlayableWorldState worldState;
 
 	public override void _Ready()
@@ -15,19 +17,42 @@ public partial class MapGenerator : Node3D
 		worldState = GetNode<PlayableWorldState>("/root/PlayableWorldState");
 		worldState.State.TerrainState.CellAdded.Subscribe(OnCellAdded);
 
-		// Generate a simple map
-		for (int q = -5; q <= 5; q++)
+		if (grid != null)
 		{
-			for (int r = -5; r <= 5; r++)
+			grid.DebugDrawRadius = MapRadius;
+		}
+		
+		// Generate a hexagonal map
+		for (int q = -MapRadius; q <= MapRadius; q++)
+		{
+			var r1 = Mathf.Max(-MapRadius, -q - MapRadius);
+			var r2 = Mathf.Min(MapRadius, -q + MapRadius);
+			for (int r = r1; r <= r2; r++)
 			{
-				if (q * q + r * r > 25) continue;
-
 				var cell = new Vector2I(q, r);
-				var terrainType = (TerrainState.TerrainType)GD.RandRange(0, 3);
+				// Weighted random terrain selection
+				TerrainState.TerrainType terrainType;
+				float rand = GD.Randf(); // Random float between 0.0 and 1.0
+
+				if (rand < 0.6f) // 60% chance for Plains
+				{
+					terrainType = TerrainState.TerrainType.Plains;
+				}
+				else if (rand < 0.8f) // 20% chance for Forest (0.6 to 0.8)
+				{
+					terrainType = TerrainState.TerrainType.Forest;
+				}
+				else if (rand < 0.9f) // 10% chance for Mountain (0.8 to 0.9)
+				{
+					terrainType = TerrainState.TerrainType.Mountain;
+				}
+				else // 10% chance for Water (0.9 to 1.0)
+				{
+					terrainType = TerrainState.TerrainType.Water;
+				}
 				worldState.State.TerrainState.AddCellAt(cell, terrainType);
 			}
-		}
-	}
+		}	}
 
 	private async System.Threading.Tasks.Task OnCellAdded((Vector2I, TerrainState.TerrainType) cellData)
 	{
